c4c2c0b54b9d3692e309a529161828966e9519fa
tiagoc0sta <tiagodpv@gmail.com>
2024-05-23 12:59:23 -0400

    fisrt ios app

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Tiago de Freitas Costa on 2024-05-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
